f12ee6e15cf72d17f92c7b13ce94c4536ac65b1f
Oesshi LAHIRI <0041345@schoolsnet.act.edu.au>
2026-05-20 09:55:20 +1000

    Update filebackuplogging.py

--- filebackuplogging.py
+++ filebackuplogging.py
@@ -1,30 +1,72 @@
 # group-project
 import os 
 import shutil 
-import hashlib 
+from abc import abc, abstractmethod
+from datetime import datetime
+
+class BackUpLogger:
+  _instance = None
+
+def __new__(cls):
+  if cls._instance is None:
+    cls._instance = super(Backkuplogger, cls).__new__(cls)
+return cls._instance
+
+def log(self, message):
+  timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S)
+  print(f"[{timestamp}] message")
+        
+class FileService(ABC):
+  @abstractmethod 
+  def sync_file(self, source, destination):
+    pass import hashlib 
 import logging 
 import time
-source_dir = r"C:\path\to\your\source"
-backup_dir = r"C:\path\to\yout\backup"
-poll_interval = 10
-log_file = "backup_utility.log" 
 
-logging.basicConfig(level=logging, INFO, format='%(asctime)s - %(levelname)s - %(message)s,
-handlers=[
-  logging.FileHandler(Log_file),
-  logging.StreamHandler()
-]
-) 
+class OSfileAdapter(FileService):
+  def sync_file(self, source, destination):
+    if not os.path.exists(source)
+    return "Error: Source is not found:
 
-def get_file_hash(filepath):
-  hasher=hashlib.md5()
-  try:
-    with open(filepath, 'rb') as f:
-      buf = f.read(65536)
-      while len(buf) > 0:
-        hasher.update(buf)
-return hasher.hexdigest()
-except Exception as e:
-logging.error(f"Error reading file {filepath}: {e}")
-return None
+   if os.path.exists(destination):
+     if os.path.getmtime(source) <= os.path.getmtime(destination)
+     return "No changes made"
+
+shutil.copy2(source,destination)
+return "Copied file"
+
+
+class Command(ABC):
+  @abstractmethod 
+def execute(self):
+  pass 
+
+class BackUpCommand(Command):
+  def __init__(self, source, destination, adapter):
+    self.source = source 
+    self.destination = destination import hashlib 
+import logging 
+import time
+    self.adapter = adapter
+    self.logger = BackupLogger()
+
+def executer(self):
+  result = self.adapter.sync_file(self.source, self.destination)
+  filename = os.path.basename(self.source)
+if result == "Copied file":
+  self.logger.log(f"Success : {filename} backed up to {self.destination}
+elif result == "No changes made":
+   self.logger.log(f"Skipped : {filename} has made no changes")
+else:
+  self.logger.log(f"Failed : {result}")
+
+class BackUpUtility:
+  def __init__(self):
+  self.queue = []
+  def add_task(self,command):
+  self.queue.append(command)
+def run_monitoring_simulation(self):
+self.logger = BackUpLogger()
+  
+  
 
