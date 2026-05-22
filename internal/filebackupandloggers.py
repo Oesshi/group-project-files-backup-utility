886b87f5d662867a7c16eaf36f56e879c5e070a2
kaiescoding <8039040@schoolsnet.act.edu.au>
2026-05-22 14:03:24 +1000

    Update filebackupandloggers.py

--- internal/filebackupandloggers.py
+++ internal/filebackupandloggers.py
@@ -59,33 +59,43 @@ c = input(backupMenu)
 
 if c == 'status':
     compare.report()
+    logging.info("Files compared.")
 
 if c == 'copy':
-    print("Works")
-    for src_dir, dirs, files in os.walk(root_src_dir):
-        dst_dir = src_dir.replace(root_src_dir, root_dst_dir, 1)
-        if not os.path.exists(dst_dir):
-            os.makedirs(dst_dir)
-        for file_ in files:
-            src_file = os.path.join(src_dir, file_)
-            dst_file = os.path.join(dst_dir, file_)
-            if os.path.exists(dst_file):
-                if os.path.samefile(src_file, dst_file):
-                    continue
-                os.remove(dst_file)
-            shutil.move(src_file, dst_dir)
-            print(f"File '{src_file}' copied and replaced successfully.")
-            logging.info("File replacement logged.")
-        compare.report()
-        
+    if os.listdir(root_src_dir):
+        checker = input(f"Every file from {root_src_dir} will be moved to {root_dst_dir}! Are you sure? Yes/No: ")
+        if checker == "Yes":
+            for src_dir, dirs, files in os.walk(root_src_dir):
+                dst_dir = src_dir.replace(root_src_dir, root_dst_dir, 1)
+                if not os.path.exists(dst_dir):
+                    os.makedirs(dst_dir)
+                for file_ in files:
+                    src_file = os.path.join(src_dir, file_)
+                    dst_file = os.path.join(dst_dir, file_)
+                    if os.path.exists(dst_file):
+                        if os.path.samefile(src_file, dst_file):
+                            continue
+                        os.remove(dst_file)
+                    shutil.copy(src_file, dst_dir)
+                    print(f"File '{src_file}' copied and replaced successfully.")
+                    logging.info("File replacement logged.")
+                compare.report()
+        else:
+            logging.info("Operation has been cancelled.")
+    else:
+        logging.info("No files to be synced.")
+
 if c == 'delete':
     removie = input("Enter the name of the file you'd like to remove, include directory: ")
     if os.path.exists(removie):
         os.remove(removie)
         print(f"File {removie} has been successfully deleted.")
-        logging.info("File deletion logged.")
+        logging.warn("File deletion logged.")
     else:
         print(f"The file does not exist.")
+        logging.error("Bad input")
+    compare.report()
 
 if c == 'exit':
+    logging.info("No changes found, exiting.")
     sys.exit()
